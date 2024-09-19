e70d655eb8ad8486d921b74207e7dc2604a23a2d
Archit301 <tambiarchit@gmail.com>
2024-09-20 03:24:50 +0530

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,10 @@ mongoose
    })
    
    
-   app.use(express.static(path.join(__dirname, '/frontend/dist')));
+   app.use(express.static(path.join(__dirname, '/Frontend/dist')));
    
    app.get('*', (req, res) => {
-     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+     res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
    })
 
 
